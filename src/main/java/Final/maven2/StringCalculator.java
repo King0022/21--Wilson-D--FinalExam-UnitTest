204bb9d6f2303eabb41dce7d484ead4b8412d633
[King] <[drewpw41@gmail.com]>
2021-06-23 13:09:28 -0700

    updtae test

--- src/main/java/Final/maven2/StringCalculator.java
+++ src/main/java/Final/maven2/StringCalculator.java
@@ -6,7 +6,10 @@ public class StringCalculator {
 	      String[] numbersArray = numbers.split(",|\n"); 
 	      for (String number : numbersArray) {
 	         if (!number.trim().isEmpty()) {
-	            returnValue += Integer.parseInt(number.trim());
+	            if (Integer.parseInt(number.trim()) < 0) {
+	            	throw new IndexOutOfBoundsException("negative numbers not allowed");
+	            }
+	        	returnValue += Integer.parseInt(number.trim());
 	         }
 	      }
 	      return returnValue;
